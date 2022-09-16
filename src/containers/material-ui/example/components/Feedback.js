bf0e9c111991fc89da458f6b371679056a78444b
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-09-17 01:17:11 +0900

    dialog

--- src/containers/material-ui/example/components/Feedback.js
+++ src/containers/material-ui/example/components/Feedback.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Box, Container, Stack } from '@mui/material';
 import Alerts from '../../../../components/material-ui/example/components/feedback/Alerts';
+import Backdrops from '../../../../components/material-ui/example/components/feedback/Backdrops';
+import Dialogs from '../../../../components/material-ui/example/components/feedback/Dialogs';
 
 
 export default () => {
@@ -19,6 +21,8 @@ export default () => {
                     <Stack spacing={3}>
                         {/* Stack으로 적당한 거리 두기 가능 */}
                         <Alerts />
+                        <Backdrops/>
+                        <Dialogs/>
                     </Stack>
                 </Container>
             </Box>
